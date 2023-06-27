bf78af357bd1fcd7c31331b00b5f90e4c0c731d0
cankut <cankutsogancilar@gmail.com>
2023-06-27 13:32:55 -0400

    made some changes

---
+++ src/Car/Ford.java
@@ -0,0 +1,7 @@
+package Car;
+
+public class Ford {
+    void method1(){
+        System.out.println("blabla");
+    }
+}
--- src/Home/SingleFamily.java
+++ src/Home/SingleFamily.java
@@ -2,8 +2,8 @@ package Home;
 
 public class SingleFamily {
     public void method1(){
-        for (int i = 0; i < 5; i++) {
-            System.out.println("blabla");
+        while(true){
+            System.out.println("Hello!");
         }
     }
 }
